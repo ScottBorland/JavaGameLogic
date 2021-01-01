fde084cb4a85fff9e606c4cce6a2c68fd5a99c62
johndoe2196 <johndoe219656@gmail.com>
2021-01-01 18:25:58 +0000

    Succesfully implement acceleration function to complete ground movement

--- scottborland/src/main/java/test/Player.java
+++ scottborland/src/main/java/test/Player.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.util.Random;
+import java.util.Vector;
 
 import math.geom2d.Point2D;
 import math.geom2d.Vector2D;
@@ -18,6 +19,9 @@ public class Player extends GameObject{
     Vector2D acceleration;
     Boolean grounded;
 
+    public double moveSpeed = 3;
+    public double runAcceleration = 4;
+
     protected int verticalInput, horizontalInput;
 
     public Player(int x, int y, ID id, Handler handler) {
@@ -30,29 +34,46 @@ public class Player extends GameObject{
     }
 
     public void update(){
-        x += velocity.x();
-        y += velocity.y();
-        position = position.plus(velocity);
-        // x = (int) position.x();
-        // y = (int) position.y();
+        x += (10 * velocity.x());
+        y += (10 * velocity.y());
+        velocity = new Vector2D(0, 0);
     }
 
     public void tick(){
         Game.angle = rotatePlayer();
-        update();
         groundMove();
+        update();
     }
 
     public void groundMove(){
-        Vector2D velocity = new Vector2D(KeyInput.horizontalInput, KeyInput.verticalInput);
-        double mag = velocity.norm();
+        Vector2D input = new Vector2D(KeyInput.horizontalInput, KeyInput.verticalInput);
+        double mag = input.norm();
         if(mag > 0){
-            double heading = velocity.angle(); 
+            double heading = input.angle(); 
             double sumAngle = heading + Math.toRadians(-Game.angle);
             Vector2D wishdir = Vector2D.createPolar(1, sumAngle);
+            accelerate(wishdir, moveSpeed, runAcceleration);
             //position = new Vector2D(10*wishdir.x(), 10*wishdir.y());
         }
     }
+
+    public void accelerate(Vector2D wishdir, double wishSpeed, double accel){
+        double addspeed;
+        double accelspeed;
+        double currentspeed;
+
+        currentspeed = velocity.dot(wishdir);
+        addspeed = wishSpeed - currentspeed;
+        if(addspeed <= 0)
+            return;
+        accelspeed = accel * wishSpeed;
+        if(accelspeed > addspeed) 
+            accelspeed = addspeed;
+        Vector2D addVel = new Vector2D (accelspeed * wishdir.x(), accelspeed * wishdir.y());
+        velocity = velocity.plus(addVel);
+        //System.out.println(velocity);
+    }
+
     public double rotatePlayer(){
         Point2D p = Game.getMousePos();
         double angle = -Game.map(p.x(), 0, Game.WIDTH, -360, 360);
